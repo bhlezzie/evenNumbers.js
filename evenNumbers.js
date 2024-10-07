f22d26898015b763cec50c248994f818e52ec309
bhlezzie <garbinbhlezzie@gmail.com>
2024-10-07 09:48:21 +0800

    Create evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,6 @@
+// evenNumbers.js
+
+// Loop through numbers from 2 to 10, incrementing by 2 each time
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i); // Log the even number to the console
+}
